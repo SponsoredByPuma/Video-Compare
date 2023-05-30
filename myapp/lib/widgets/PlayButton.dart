f74c344888ce28f5d159bf65952cb1ca484091d2
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-05-30 15:57:38 +0200

    added a watcher to both controllers, looks if video reached end

--- myapp/lib/widgets/PlayButton.dart
+++ myapp/lib/widgets/PlayButton.dart
@@ -31,6 +31,36 @@ class _PlayButtonState extends State<PlayButton> {
     });
   }
 
+  void addWatcherToController() {
+    if (widget.firstVideoController!.value.duration >=
+        widget.secondVideoController!.value.duration) {
+      widget.firstVideoController!.addListener(() {
+        setState(() {
+          if (widget.firstVideoController!.value.duration ==
+              widget.firstVideoController!.value.position) {
+            setState(() {
+              _isPlaying = false;
+              widget.firstVideoController!.seekTo(Duration.zero);
+              widget.secondVideoController!.seekTo(Duration.zero);
+            });
+          }
+        });
+      });
+    } else {
+      widget.secondVideoController!.addListener(() {
+        setState(() {
+          if (widget.secondVideoController!.value.duration ==
+              widget.secondVideoController!.value.position) {
+            setState(() {
+              _isPlaying = false;
+              widget.firstVideoController!.seekTo(Duration.zero);
+              widget.secondVideoController!.seekTo(Duration.zero);
+            });
+          }
+        });
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +70,7 @@ class _PlayButtonState extends State<PlayButton> {
         size: 48,
       ),
       onPressed: () {
+        addWatcherToController();
         _togglePlay();
       },
     );
