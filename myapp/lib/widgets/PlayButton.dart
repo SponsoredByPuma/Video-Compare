40095dedc2f3eaa5099d6c137d97b645fbe8d9ef
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-06-08 12:31:31 +0200

    add dispose function + watcher

--- myapp/lib/widgets/PlayButton.dart
+++ myapp/lib/widgets/PlayButton.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:myapp/home/home_view.dart';
 import 'package:video_player/video_player.dart';
 
@@ -28,20 +27,29 @@ class PlayButton extends StatefulWidget {
 }
 
 class _PlayButtonState extends State<PlayButton> {
-  bool _togglePlay() {
-    bool isPlaying = widget.firstVideoController.value.isPlaying ||
+  late bool _isPlaying;
+
+  @override
+  void initState() {
+    super.initState();
+    _isPlaying = widget.firstVideoController.value.isPlaying ||
         widget.secondVideoController.value.isPlaying;
-    setState(() {
-      isPlaying = !isPlaying;
-      if (isPlaying) {
-        widget.firstVideoController.play();
-        widget.secondVideoController.play();
-      } else {
-        widget.firstVideoController.pause();
-        widget.secondVideoController.pause();
-      }
-    });
-    return isPlaying;
+    double firstVideoDuration = widget.homecontroller.getFirstVideoEnd() -
+        widget.homecontroller.getFirstVideoStart();
+    double secondVideoDuration = widget.homecontroller.getSecondVideoEnd() -
+        widget.homecontroller.getSecondVideoStart();
+    addWatcherToController(
+        firstVideoDuration: firstVideoDuration,
+        secondVideoDuration: secondVideoDuration,
+        firstVideoEndPoint: widget.homecontroller.getFirstVideoEnd(),
+        secondVideoEndPoint: widget.homecontroller.getSecondVideoEnd());
+  }
+
+  @override
+  void dispose() {
+    widget.firstVideoController.removeListener(_watcherFirstVideo);
+    widget.secondVideoController.removeListener(_watcherSecondVideo);
+    super.dispose();
   }
 
   void addWatcherToController({
@@ -51,78 +59,71 @@ class _PlayButtonState extends State<PlayButton> {
     required double firstVideoEndPoint,
   }) {
     if (firstVideoDuration >= secondVideoDuration) {
-      widget.firstVideoController.addListener(
-        () {
-          if (firstVideoEndPoint <=
-              widget.firstVideoController.value.position.inMilliseconds) {
-            widget.firstVideoController.pause();
-            widget.secondVideoController.pause();
-            widget.firstVideoController.seekTo(
-                Duration(milliseconds: widget.firstVideoStartPoint.toInt()));
-            widget.secondVideoController.seekTo(
-                Duration(milliseconds: widget.secondVideoStartPoint.toInt()));
-          } else {
-            if (secondVideoEndPoint <=
-                widget.secondVideoController.value.position.inMilliseconds) {
-              widget.secondVideoController.pause();
-            }
-          }
-        },
-      );
+      widget.firstVideoController.addListener(_watcherFirstVideo);
     } else {
-      widget.secondVideoController.addListener(
-        () {
-          setState(
-            () {
-              if (secondVideoEndPoint <=
-                  widget.secondVideoController.value.position.inMilliseconds) {
-                widget.firstVideoController.pause();
-                widget.secondVideoController.pause();
-                widget.firstVideoController.seekTo(Duration(
-                    milliseconds: widget.firstVideoStartPoint.toInt()));
-                widget.secondVideoController.seekTo(Duration(
-                    milliseconds: widget.secondVideoStartPoint.toInt()));
-              } else {
-                if (firstVideoEndPoint <=
-                    widget.firstVideoController.value.position.inMilliseconds) {
-                  widget.firstVideoController.pause();
-                }
-              }
-            },
-          );
-        },
-      );
+      widget.secondVideoController.addListener(_watcherSecondVideo);
+    }
+  }
+
+  void _watcherFirstVideo() {
+    if (mounted) {
+      setState(() {
+        if (widget.firstVideoController.value.position.inMilliseconds >=
+            widget.homecontroller.getFirstVideoEnd().toInt()) {
+          _isPlaying = false;
+          widget.firstVideoController.pause();
+          widget.secondVideoController.pause();
+          widget.firstVideoController.seekTo(
+              Duration(milliseconds: widget.firstVideoStartPoint.toInt()));
+          widget.secondVideoController.seekTo(
+              Duration(milliseconds: widget.secondVideoStartPoint.toInt()));
+        } else if (widget.secondVideoController.value.position.inMilliseconds >=
+            widget.homecontroller.getSecondVideoEnd().toInt()) {
+          widget.secondVideoController.pause();
+        }
+      });
+    }
+  }
+
+  void _watcherSecondVideo() {
+    if (mounted) {
+      setState(() {
+        if (widget.secondVideoController.value.position.inMilliseconds >=
+            widget.homecontroller.getSecondVideoEnd().toInt()) {
+          _isPlaying = false;
+          widget.firstVideoController.pause();
+          widget.secondVideoController.pause();
+          widget.firstVideoController.seekTo(
+              Duration(milliseconds: widget.firstVideoStartPoint.toInt()));
+          widget.secondVideoController.seekTo(
+              Duration(milliseconds: widget.secondVideoStartPoint.toInt()));
+        } else if (widget.firstVideoController.value.position.inMilliseconds >=
+            widget.homecontroller.getFirstVideoEnd().toInt()) {
+          widget.firstVideoController.pause();
+        }
+      });
     }
   }
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    double firstVideoDuration = widget.homecontroller.getFirstVideoEnd() -
-        widget.homecontroller.getFirstVideoStart();
-    double secondVideoDuration = widget.homecontroller.getSecondVideoEnd() -
-        widget.homecontroller.getSecondVideoStart();
-
-    addWatcherToController(
-        firstVideoDuration: firstVideoDuration,
-        secondVideoDuration: secondVideoDuration,
-        firstVideoEndPoint: widget.homecontroller.getFirstVideoEnd(),
-        secondVideoEndPoint: widget.homecontroller.getSecondVideoEnd());
-    bool isPlaying = widget.firstVideoController.value.isPlaying ||
-        widget.secondVideoController.value.isPlaying;
-
     return IconButton(
       icon: Icon(
-        isPlaying ? Icons.pause : Icons.play_arrow,
+        _isPlaying ? Icons.pause : Icons.play_arrow,
         size: 48,
         color: Theme.of(context).primaryColor,
       ),
       onPressed: () {
-        isPlaying = _togglePlay();
+        setState(() {
+          _isPlaying = !_isPlaying;
+          if (_isPlaying) {
+            widget.firstVideoController.play();
+            widget.secondVideoController.play();
+          } else {
+            widget.firstVideoController.pause();
+            widget.secondVideoController.pause();
+          }
+        });
       },
     );
   }
