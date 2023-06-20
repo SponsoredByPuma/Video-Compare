5c6b2a5ce15f99a8664261c67d100eb82425a4e3
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-06-20 14:14:10 +0200

    removed unnecessary imports

--- myapp/lib/home/home_controller.dart
+++ myapp/lib/home/home_controller.dart
@@ -10,8 +10,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:video_trimmer/video_trimmer.dart';
 
 import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
 import 'package:path/path.dart';
 
 class HomeControllerImplmentation extends HomeController {
