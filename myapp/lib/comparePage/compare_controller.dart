32139bebb06cf9d2553ad567f63bec207bd68a77
SponsoredByPuma <92574150+SponsoredByPuma@users.noreply.github.com>
2023-06-28 14:45:18 +0200

    add about page

--- myapp/lib/comparePage/compare_controller.dart
+++ myapp/lib/comparePage/compare_controller.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:myapp/comparePage/LanguageService.dart';
-import 'package:myapp/comparePage/compare_model.dart';
-import 'package:myapp/comparePage/compare_view.dart';
+import 'package:vison/comparePage/LanguageService.dart';
+import 'package:vison/comparePage/compare_model.dart';
+import 'package:vison/comparePage/compare_view.dart';
 import 'package:get/get.dart';
 import 'dart:async';
 import 'package:path_provider/path_provider.dart';
--- myapp/lib/widgets/AboutWidget.dart
+++ myapp/lib/widgets/AboutWidget.dart
@@ -1,33 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AboutWidget extends StatelessWidget {
   const AboutWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Stack(
       children: [
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'About',
-                style: TextStyle(fontSize: 24),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'This is a sample app that allows you to compare videos.',
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Version 1.0.0',
-                style: TextStyle(fontSize: 16),
-              ),
-            ],
+        const Align(
+          alignment: Alignment(0, -0.95),
+          child: Text(
+            "Vison",
+            style: TextStyle(fontSize: 26),
           ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 3.0),
+          child: Align(
+            alignment: Alignment(-0.9, -0.85),
+            child: Text(
+              AppLocalizations.of(context)?.aboutTheTeam ?? '',
+              style: const TextStyle(fontSize: 26),
+            ),
+          ),
+        ),
+        Align(
+          alignment: const Alignment(-0.8, -0.725),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              AppLocalizations.of(context)?.aboutTheTeamText ?? '',
+              style: const TextStyle(fontSize: 15),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 3.0),
+          child: Align(
+            alignment: const Alignment(-0.9, -0.575),
+            child: Text(
+              AppLocalizations.of(context)?.aboutTheApp ?? '',
+              style: const TextStyle(fontSize: 26),
+            ),
+          ),
+        ),
+        Align(
+          alignment: const Alignment(-0.8, -0.45),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              AppLocalizations.of(context)?.aboutTheAppText ?? '',
+              style: const TextStyle(fontSize: 15),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 3.0),
+          child: Align(
+            alignment: const Alignment(-0.9, -0.3),
+            child: Text(
+              AppLocalizations.of(context)?.aboutTheName ?? '',
+              style: const TextStyle(fontSize: 26),
+            ),
+          ),
+        ),
+        Align(
+          alignment: const Alignment(-0.8, -0.125),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              AppLocalizations.of(context)?.aboutTheNameText ?? '',
+              style: const TextStyle(fontSize: 15),
+            ),
+          ),
+        ),
+        Align(
+          alignment: const Alignment(0, 0.7),
+          child: Image.asset('assets/images/BisonTransparent.png',
+              width: 300, height: 300),
         ),
       ],
     );
