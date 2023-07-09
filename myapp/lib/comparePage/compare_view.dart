781c4798d1e3750ad161678623a51e2db4dfa0ff
lukas <lukasdoetterl@hotmail.com>
2023-07-09 12:30:56 +0200

    removed useless Code and changed small padding

--- myapp/lib/comparePage/compare_view.dart
+++ myapp/lib/comparePage/compare_view.dart
@@ -14,7 +14,6 @@ import 'package:vison/widgets/HorizontalContainer.dart';
 import 'package:vison/widgets/VerticalContainer.dart';
 import 'package:vison/widgets/VideoProgressSlider.dart';
 import 'package:vison/widgets/VideoTrimmer.dart';
-import 'package:get/get.dart';
 import 'package:smooth_video_progress/smooth_video_progress.dart';
 import 'package:video_player/video_player.dart';
 import 'dart:io';
@@ -257,103 +256,7 @@ class CompareView extends ConsumerWidget {
             },
             themeSwitch: () {
               controller.switchColorMode();
-              controller.getLightMode()
-                  ? Get.changeTheme(
-                      ThemeData(
-                        scaffoldBackgroundColor:
-                            const Color.fromRGBO(70, 158, 209, 1),
-                        appBarTheme: const AppBarTheme(
-                          backgroundColor: Color.fromRGBO(70, 158, 209, 1),
-                          iconTheme: IconThemeData(color: Colors.black),
-                        ),
-                        elevatedButtonTheme: ElevatedButtonThemeData(
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.black,
-                            backgroundColor:
-                                const Color.fromARGB(255, 82, 224, 153),
-                          ),
-                        ),
-                        primaryColor: Colors.black,
-                        sliderTheme: const SliderThemeData(
-                          activeTrackColor: Color.fromARGB(248, 88, 10, 161),
-                          inactiveTrackColor: Color.fromARGB(204, 148, 10, 10),
-                          trackHeight: 14,
-                          thumbShape: RoundSliderThumbShape(
-                              enabledThumbRadius: 12, pressedElevation: 10),
-                          thumbColor: Color.fromARGB(255, 255, 255, 255),
-                          trackShape: RectangularSliderTrackShape(),
-                          showValueIndicator: ShowValueIndicator.always,
-                          valueIndicatorColor: Color.fromARGB(255, 0, 0, 0),
-                          valueIndicatorTextStyle: TextStyle(
-                              color: Color.fromARGB(255, 255, 255, 255)),
-                        ),
-                        primaryTextTheme:
-                            Typography(platform: TargetPlatform.iOS).black,
-                        textTheme:
-                            Typography(platform: TargetPlatform.iOS).black,
-                        canvasColor: Colors.white,
-                        popupMenuTheme:
-                            const PopupMenuThemeData(color: Colors.white),
-                        inputDecorationTheme: const InputDecorationTheme(
-                          labelStyle:
-                              TextStyle(color: Colors.black, fontSize: 26),
-                        ),
-                        dialogTheme: const DialogTheme(
-                          backgroundColor: Colors.white,
-                          contentTextStyle:
-                              TextStyle(color: Colors.black, fontSize: 22),
-                        ),
-                      ),
-                    )
-                  : Get.changeTheme(
-                      ThemeData(
-                        scaffoldBackgroundColor:
-                            const Color.fromARGB(255, 67, 13, 117),
-                        appBarTheme: const AppBarTheme(
-                          backgroundColor: Color.fromARGB(255, 67, 13, 117),
-                        ),
-                        elevatedButtonTheme: ElevatedButtonThemeData(
-                          style: ElevatedButton.styleFrom(
-                            foregroundColor: Colors.white,
-                            backgroundColor:
-                                const Color.fromARGB(255, 216, 99, 67),
-                          ),
-                        ),
-                        primaryColor: Colors.white,
-                        sliderTheme: const SliderThemeData(
-                          activeTrackColor: Color.fromARGB(249, 161, 151, 10),
-                          inactiveTrackColor: Color.fromARGB(204, 14, 161, 117),
-                          trackHeight: 14,
-                          thumbShape: RoundSliderThumbShape(
-                              enabledThumbRadius: 12, pressedElevation: 10),
-                          thumbColor: Color.fromARGB(255, 0, 0, 0),
-                          trackShape: RectangularSliderTrackShape(),
-                          showValueIndicator: ShowValueIndicator.always,
-                          valueIndicatorColor: Colors.amber,
-                          valueIndicatorTextStyle:
-                              TextStyle(color: Colors.black),
-                        ),
-                        primaryTextTheme:
-                            Typography(platform: TargetPlatform.iOS).white,
-                        textTheme:
-                            Typography(platform: TargetPlatform.iOS).white,
-                        canvasColor: Colors.black,
-                        popupMenuTheme:
-                            const PopupMenuThemeData(color: Colors.black),
-                        iconTheme: const IconThemeData(color: Colors.white),
-                        inputDecorationTheme: const InputDecorationTheme(
-                          labelStyle:
-                              TextStyle(color: Colors.white, fontSize: 26),
-                        ),
-                        dialogTheme: const DialogTheme(
-                          backgroundColor: Colors.white,
-                          contentTextStyle:
-                              TextStyle(color: Colors.black, fontSize: 22),
-                          titleTextStyle:
-                              TextStyle(color: Colors.black, fontSize: 24),
-                        ),
-                      ),
-                    );
+              controller.getLightMode();
             },
           ),
         ],
@@ -479,7 +382,7 @@ class CompareView extends ConsumerWidget {
                   ),
                 ),
                 Expanded(
-                  flex: 2,
+                  flex: 1,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
